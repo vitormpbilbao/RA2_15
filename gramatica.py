1096a54f74254c235ee4703b4636b1e4622dc7d8
Vitor <vitaodamassa35@gmail.com>
2026-04-23 18:27:35 -0300

    def calcularFollow feita

--- gramatica.py
+++ gramatica.py
@@ -68,7 +68,35 @@ def calcularFirst(gramatica):
     return firsts
 
 def calcularFollow(gramatica, firsts):
-    pass
+    follows = {nt: set() for nt in gramatica}
+    follows["Programa"].add("$")
+    teve_mudanca = True
+    while teve_mudanca:
+        teve_mudanca = False
+        for nt_pai, regras in gramatica.items():
+            for regra in regras:
+                for i, simbolo in enumerate(regra):
+                    if simbolo in gramatica:
+                        tamanho_antigo = len(follows[simbolo])
+                        #se tem a direita
+                        if i + 1 < len(regra):
+                            proximo_simbolo = regra[i + 1]
+                            
+                            if proximo_simbolo not in gramatica: #éterminal
+                                follows[simbolo].add(proximo_simbolo)
+                            else: #n terminal
+                                firsts_do_vizinho = firsts[proximo_simbolo]
+                                follows[simbolo].update(firsts_do_vizinho - {"EPSILON"})
+                                
+                                if "EPSILON" in firsts_do_vizinho:
+                                    follows[simbolo].update(follows[nt_pai])
+                        #herda o follow do pai se for o último símbolo
+                        else:
+                            follows[simbolo].update(follows[nt_pai])
+                        if len(follows[simbolo]) > tamanho_antigo:
+                            teve_mudanca = True
+
+    return follows
 
 def construirTabelaLL1(gramatica, firsts, follows):
     pass
